e9e693682cf1cdae3f8dca459698843fb1e879a8
Brian <briancortesperezcejuela@gmail.com>
2026-06-03 09:49:35 +0200

    Aplications

---
+++ src/main/java/com/example/concesionario/ListaAplication.java
@@ -0,0 +1,17 @@
+package com.example.concesionario;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ListaAplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
+
